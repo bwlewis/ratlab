0ba8fc8150498069ce268b80da755cb40f6bea49
B. W. Lewis <bwaynelewis@gmail.com>
2009-12-31 02:02:00 -0500

    Tweaked subsasgn a bit, still not 100%. Also can't append columns nor rows yet.

--- @dataframe/subsasgn.m
+++ @dataframe/subsasgn.m
@@ -12,11 +12,15 @@ function x = subsasgn(obj, S, value)
         if(length(idx)==1)
 % No row indices; replace entire column(s)
           if(iscell(value)) obj.data{1,idx{1}(j)} = value{j};
+          elseif(strcmp("dataframe",class(value))) 
+            obj.data{1,idx{1}(j)} = value.data{j};
           else obj.data{1,idx{1}(j)} = value(:,j);
           end
         else
-% Row subset specified; general replacement
+% Row subset specified; general replacement XXX not quite right yet
           if(iscell(value)) obj.data{1,idx{2}(j)}(idx{1}) = value{1,j};
+          elseif(strcmp("dataframe",class(value))) 
+            obj.data{1,idx{2}(j)}(idx{1}) = value.data{j};
           else obj.data{1,idx{2}(j)}(idx{1}) = value(:,j);
           end
         end
