27086aa8f82c87a2cb5497180397555c8a04b7a5
B. W. Lewis <bwaynelewis@gmail.com>
2010-01-07 13:05:52 -0500

    Fixed up dataframe subsref; subsasgn and display still not quite there.

--- @dataframe/subsref.m
+++ @dataframe/subsref.m
@@ -7,19 +7,22 @@ function x = subsref(obj, S)
     case "()"
       idx = S.subs;
 % R-style dataframe indexing behavior
-      n = length(idx{length(idx)});
+      l = length(idx);
+      n = length(idx{l});
+      rownames = obj.rownames;
       data = cell(1, n);
       colnames = cell(1,n);
       for j=1:n
-        if (length(idx)==1)
+        if (l==1)
 % No row indices; select full columns
           data{1,j} = obj.data{1,idx{1}(j)};
           colnames{1,j} = obj.colnames{1,idx{1}(j)};
         else
           data{1,j} = obj.data{1,idx{2}(j)}(idx{1});
           colnames{1,j} = obj.colnames{1,idx{2}(j)};
+          rownames = obj.rownames{1,idx{1}};
         end
       end
-      x = dataframe(colnames,data);
+      x = dataframe(colnames,rownames,data);
   endswitch
 endfunction
