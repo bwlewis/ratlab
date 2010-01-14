1b98a2555f374d192f82b92db34daf3824be6c8f
Patrick Shields <patrick.m.shields@gmail.com>
2010-01-14 13:04:26 -0700

    Quick flip of the ERRORLEVEL. Was set backwards.

--- ratlab_run.bat
+++ ratlab_run.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 IF NOT "%1"=="" SET R_HOME=%1 ELSE SET R_HOME=c:\Program Files\R\R-2.10.1\
 IF NOT EXIST "%R_HOME%\bin\R.lib" COPY "%R_HOME%\bin\R.dll" "%R_HOME%\bin\R.lib"
-IF ERRORLEVEL 0 GOTO bigerror
+IF ERRORLEVEL 1 GOTO bigerror
 SET PATH=%PATH%;%R_HOME%\bin
 IF NOT EXIST ratlab_internal.mex* GOTO freshstart
 matlab
